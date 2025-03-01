118e40afa7a6f07053b5dca746c54ee03d026581
naufalharitsprasetia <naufalharisprasetia@gmail.com>
2025-03-01 23:56:47 +0800

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -51,7 +51,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             BlogSeeder::class,
             DepartementSeeder::class,
-            UKMSeeder::class,
         ]);
     }
 }
